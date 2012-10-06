f0180d2736bead25c7b3b80af69e2db8009c7742
M. Kucharskov <M.Kucharskov@onet.pl>
2012-10-06 20:23:04 +0200

    System sprawdzania pliku config.php

--- administration/index.php
+++ administration/index.php
@@ -1,4 +1,13 @@
-<?php require "../config.php"; ?>
+<?php 
+if(!file_exists('../config.php'))
+{
+echo "<div style='font-family: Helvetica Neue,Helvetica,Arial,sans-serif; font-size: 13px; width: 350px; margin: 0 auto; text-align: center; color: #FFFFFF; border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25); border-radius: 4px 4px 4px 4px; border-style: solid; border-width: 1px; box-shadow: 0 1px 0 rgba(255, 255, 255, 0.25) inset; margin-bottom: 18px; padding: 7px 15px; position: relative; background-color: #C43C35; background-image: -moz-linear-gradient(center top , #EE5F5B, #C43C35); background-repeat: repeat-x; border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25); text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);'>";
+echo "File <b>config.php</b> required by MiniS CMS has not been found!";
+echo "</div>";
+die;
+}
+require "../config.php";
+?>
 <!DOCTYPE html>
 <html>
 <head>
