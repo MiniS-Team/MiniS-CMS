454adb96dfe406b83efbbb134dbbf27054339218
M. Kucharskov <M.Kucharskov@onet.pl>
2012-10-13 20:46:17 +0200

    Rosyjskie tłumaczenie

---
+++ lang/russkij.php
@@ -0,0 +1,64 @@
+<?php
+/*
+#
+# Языковый файл «Русский» для скрипта MiniS CMS
+# M. Kucharskov & MiniS Team
+#
+*/
+
+$lang['CMS'] = "MiniS CMS";
+$lang['LOG'] = "Логин";
+$lang['PASS'] = "Пароль";
+$lang['LOGIN'] = "Вход";
+$lang['LOGOUT'] = "Выход";
+$lang['PVIEW'] = "Просмотр";
+$lang['PA'] = "Панель администратора";
+$lang['COM'] = "Коммюнике";
+$lang['REDIR'] = "Перенаправление";
+$lang['REDIR_'] = "Перенаправление...";
+$lang['LOGIN_OK'] = "Успешный вход!";
+$lang['LOGIN_NO'] = "Прием данных невозможен";
+$lang['LOGIN_WAIT'] = "Ждите перенаправления";
+$lang['MENU_1'] = "Настройки";
+$lang['MENU_2'] = "Просмотр сайтов";
+$lang['MENU_3'] = "Редакция сайта";
+$lang['MENU_4'] = "Добавить сайт";
+$lang['OK'] = "окей!";
+$lang['NO_FILE'] = "Файл отсутствует!";
+$lang['SK_INFO'] = "Информация о системе";
+$lang['VER_LOC'] = "Локальная версия";
+$lang['VER'] = "Новейшая версия";
+$lang['SK_ST'] = "Состояние скрипта";
+$lang['SK_AK'] = "АКТУАЛЬНЫЙ";
+$lang['SK_NAK'] = "НЕАКТУАЛЬНЫЙ";
+$lang['SK_PNW'] = "Скачать&nbsp;новейшую&nbsp;версию";
+$lang['SK_ZM'] = "Список изменений";
+$lang['L_PLK'] = "Список файлов";
+$lang['PLK_ZAP_OK'] ="Успешно записано!";
+$lang['PLK_ZAP_NO'] ="Файл о таком названии существует!";
+$lang['ET_NS'] ="Название сайта";
+$lang['ET_ZAW'] ="Содержание";
+$lang['ET_ZAP'] ="Записать";
+$lang['ET_AN'] ="Отменить";
+$lang['ET_COF'] ="Назад";
+$lang['ET_WYM'] ="(требуется)";
+$lang['ET_BD_NAZW'] = "Название файла может содержать только: буквы, цифры а также симбол '_'!";
+$lang['ET_BD_NO_NAZW'] = "Ввести название файла!";
+
+$lang['INSTAL_1'] = "Лицензия";
+$lang['INSTAL_2'] = "Выбор языка";
+$lang['INSTAL_NOLANGSELECTED'] = "Выбрать язык чтобы перейти.";
+$lang['INSTAL_3'] = "Текст файлов";
+$lang['INSTAL_4'] = "Конфигурация";
+$lang['INSTAL_5'] = "Окончание";
+$lang['INSTAL_LANG'] = "Выберите язык";
+$lang['INSTAL_LANG_1'] = "Записано!";
+$lang['INSTAL_LANG_2'] = "Подождать на загрузку языка!";
+$lang['INSTAL_REF'] = "Обновить";
+$lang['INSTAL_ACC'] = "Принимаю";
+$lang['INSTAL_SET'] = "Настроить";
+$lang['INSTAL_NEXT'] = "Дальше";
+
+GLOBAL $lang
+
+?>
