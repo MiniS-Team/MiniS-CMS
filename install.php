709c92ecd28ad556f4fcc6edfe1c4d46d48bd533
M. Kucharskov <M.Kucharskov@onet.pl>
2012-10-20 19:24:33 +0200

    Poprawiono wsparcie starszych przeglądarek (w tym IE)

--- install.php
+++ install.php
@@ -1,7 +1,9 @@
 ﻿<?php
 if(file_exists('.lock')) {
+	echo "<div style='text-align: center; margin: 0 auto;'>";
 	echo "<div style='font-family: Helvetica Neue,Helvetica,Arial,sans-serif; font-size: 13px; width: 350px; margin: 0 auto; text-align: center; color: #FFFFFF; border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25); border-radius: 4px 4px 4px 4px; border-style: solid; border-width: 1px; box-shadow: 0 1px 0 rgba(255, 255, 255, 0.25) inset; margin-bottom: 18px; padding: 7px 15px; position: relative; background-color: #C43C35; background-image: -moz-linear-gradient(center top , #EE5F5B, #C43C35); background-repeat: repeat-x; border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25); text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);'>";
 	echo "Delete <b>.lock</b> file to run installer!";
+	echo "</div>";
 	echo "</div>";
 	die;
 }
