01291f4d0c07a6353d110c0d80a199c41e987f6d
M. Kucharskov <M.Kucharskov@onet.pl>
2012-11-06 20:03:55 +0100

    Dodanie przełącznika Mobilna-Pełna wersja

--- templates/Default/footer.php
+++ templates/Default/footer.php
@@ -1,12 +1,14 @@
 	<!-- STOPKA NIE USUWAĆ! -->
 	<div id="footer">
+	
 	<?php
 	echo $lang['CMS']; 
 	$version = file_get_contents('version.kucharskov');
 	echo " <b>".base64_decode($version)."</b>";
 	?>
 	by <a href="http://Kucharskov.cba.pl" target="_blank">M. Kucharskov</a> & MiniS Team<br>
-	<img src="img/MiniS_small.png" alt="">
+	<img src="img/MiniS_small.png" alt=""><br>
+	<a href="index.php?mobile=1" id="mobile"><?php echo $lang['SITE_MOBILE'] ?></a>
 	</div>
 	<!-- KONIEC STOPKI -->
 	</div>
