d59cdf22fd9c24d0aae481af0dd48211d1571925
M. Kucharskov <M.Kucharskov@onet.pl>
2012-11-17 22:18:25 +0100

    Poprawka spójności kodu

--- templates/Default/main.php
+++ templates/Default/main.php
@@ -2,20 +2,18 @@
 
 	<!-- POZIOME MENU -->
 	<?php
-	if($config['panel_poziomo'])
-	{
-	$pozioma_tresc = file_get_contents('pages/'.$config['PoziomyPanelText']);
-	echo '<div id="panel_poziomo">'.$pozioma_tresc.'</div>';
+	if($config['panel_poziomo']) {
+		$pozioma_tresc = file_get_contents('pages/'.$config['PoziomyPanelText']);
+		echo '<div id="panel_poziomo">'.$pozioma_tresc.'</div>';
 	}
 	?>
 	<!-- POZIOME MENU KONIEC -->
 	
 	<!-- LEWE MENU -->
 	<?php
-	if($config['panel_lewo'])
-	{
-	$lewa_tresc = file_get_contents('pages/'.$config['LewyPanelText']);
-	echo '<div id="panel_lewo">'.$lewa_tresc.'</div>';
+	if($config['panel_lewo']) {
+		$lewa_tresc = file_get_contents('pages/'.$config['LewyPanelText']);
+		echo '<div id="panel_lewo">'.$lewa_tresc.'</div>';
 	}
 	?>
 	<!-- LEWE MENU KONIEC -->
@@ -30,10 +28,9 @@
 
 	<!-- PRAWE MENU -->
 	<?php
-	if($config['panel_prawo'])
-	{
-	$prawa_tresc = file_get_contents('pages/'.$config['PrawyPanelText']);
-	echo '<div id="panel_prawo">'.$prawa_tresc.'</div>';
+	if($config['panel_prawo']) {
+		$prawa_tresc = file_get_contents('pages/'.$config['PrawyPanelText']);
+		echo '<div id="panel_prawo">'.$prawa_tresc.'</div>';
 	}
 	?>
 	<!-- PRAWE MENU KONIEC -->
--- templates/Mobile/main.php
+++ templates/Mobile/main.php
@@ -1,10 +1,9 @@
 <?php include 'header.php'; ?>
 
 	<?php
-	if($config['panel_poziomo'])
-	{
-	$pozioma_tresc = file_get_contents('pages/'.$config['PoziomyPanelText']);
-	echo '<div id="menu_top">'.$pozioma_tresc.'</div>';
+	if($config['panel_poziomo']) {
+		$pozioma_tresc = file_get_contents('pages/'.$config['PoziomyPanelText']);
+		echo '<div id="menu_top">'.$pozioma_tresc.'</div>';
 	}
 	?>
 	
@@ -15,14 +14,14 @@
 
 	<div id="box">
 		<?php
-		if($config['panel_lewo']){
+		if($config['panel_lewo']) {
 			$lewa_tresc = file_get_contents('pages/'.$config['LewyPanelText']);
 			echo '<div class="box_menu">'.$lewa_tresc.'</div>';
 		}
 		?>
 		
 		<?php
-		if($config['panel_prawo']){
+		if($config['panel_prawo']) {
 			$prawa_tresc = file_get_contents('pages/'.$config['PrawyPanelText']);
 			echo '<div class="box_menu">'.$prawa_tresc.'</div>';
 		}
